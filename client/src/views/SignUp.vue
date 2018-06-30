cfc51bcbc7b4c125140d312fe5c2541e60ada598
mrandrewlopez <andrewlopezcodes@gmail.com>
2018-06-29 22:00:57 -0400

    Initial commit

---
+++ client/src/views/SignUp.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column align-center>
+      <v-form v-model="valid">
+          <v-text-field
+            v-model="user.username"
+            :rules="notEmptyRules"
+            label="Username"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="user.displayName"
+            :rules="notEmptyRules"
+            label="Display Name"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="user.password"
+            :rules="notEmptyRules"
+            label="password"
+            type="password"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="user.confirmPassword"
+            :rules="notEmptyRules"
+            label="Confirm Password"
+            type="password"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="user.imageURL"
+            :rules="notEmptyRules"
+            label="Image Url"
+            required
+          ></v-text-field>
+      </v-form>
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+
+<script>
+export default{
+  name: 'signup',
+  data: () => ({
+    user: {
+      username: '',
+      password: '',
+      confirmPassword: '',
+      displayName: '',
+      imageURL: '',
+    },
+  }),
+};
+</script>
